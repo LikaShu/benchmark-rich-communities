5ecc2c3072dc81014667a3e863754838bb1f86d6
LikaShu <142459648+LikaShu@users.noreply.github.com>
2023-12-20 19:32:55 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,38 +1,14 @@
-
-
-def main():
-        print(f"Random Number: {item}")
-if __name__ == "__main__":
-    data = generate_random_data()
-    for item in data:
-import random
-
-
-def generate_random_data():
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
-    main()    public static void main(String[] args) {
-public class RandomDataGenerator {
-            System.out.println("Random Number: " + item);
-}
-
-
-    }
-        }
-            data[i] = (int) (Math.random() * 100) + 1;
-        for (int i = 0; i < 10; i++) {
-        int[] data = new int[10];
-        for (int item : data) {
-        }
-    data = [random.randint(1, 100) for _ in range(10)]
-
-import random
-
-    for item in data:
-def generate_random_data():
-    data = generate_random_data()
     main()
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+
         print(f"Random Number: {item}")
-if __name__ == "__main__":
-    return data
 def main():
+    data = generate_random_data()
+    return data
+
+
+def generate_random_data():
+
+if __name__ == "__main__":
+import random
